7edb985d06e84eba0fcb1ec0d79251a8193a7ce9
= <=>
2023-12-17 19:02:47 -0600

    Se agrega el servicio para el Toast en la pagina de cuentas espejo

--- src/main.js
+++ src/main.js
@@ -1,26 +1,26 @@
 // Importa el archivo CSS principal para los estilos de la aplicación.
+import '/node_modules/primeflex/primeflex.css'
 import './assets/main.css'
 import "primevue/resources/themes/lara-light-green/theme.css";
+import 'primeicons/primeicons.css'
 
 // Importa funciones y objetos necesarios de Vue.
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { plugin, defaultConfig } from '@formkit/vue'
-import { useToast } from 'vue-toast-notification'
 import config from '../formkit.config'
 
 import PrimeVue from 'primevue/config';
-import 'primeicons/primeicons.css'
 
 
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
-
-
-
-
+import Dialog from 'primevue/dialog'
+import Password from 'primevue/password'
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice'
 
 
 // Importa el componente principal de la aplicación.
@@ -32,25 +32,22 @@ import router from './router'
 // Importa el estilo de los mensajes de tostada (toast).
 // import "vue-toast-notification/dist/theme-sugar.css"
 
-// Crea una instancia de vue-toast-notification para manejar mensajes de tostada.
-const $toast = useToast({
-    duration: 5000, // Duración predeterminada de 5 segundos para los mensajes de tostada.
-    position: 'top-right' // Posición predeterminada de los mensajes de tostada en la esquina superior derecha.
-})
 
 // Crea una aplicación Vue.
 const app = createApp(App)
 
-// Proporciona la instancia de vue-toast-notification como 'toast' a toda la aplicación.
-app.provide('toast', $toast)
-
+app.use(ToastService)
 app.use(PrimeVue, {
     unstyled: false
 });
+
 app.component('DataTable', DataTable)
 app.component('Column', Column)
 app.component('InputText', InputText)
 app.component('Button', Button)
+app.component('Dialog', Dialog)
+app.component('Password', Password)
+app.component('Toast', Toast)
 
 
 // Usa el sistema de estado global Pinia para manejar el estado de la aplicación.
@@ -58,7 +55,7 @@ app.use(createPinia())
 
 // Usa el plugin de @formkit/vue para manejar formularios en la aplicación.
 // También se le pasa la configuración predeterminada y la configuración específica de la aplicación (config).
-app.use(plugin, defaultConfig(config))
+//app.use(plugin, defaultConfig(config))
 
 // Usa el enrutador definido en router.js para manejar la navegación de la aplicación.
 app.use(router)
